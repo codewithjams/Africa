1a92f1412d5f6e71cf696233538aaf1f84896475
ritwikjamuar <ritwikjamuar@gmail.com>
2021-05-04 19:58:01 +0530

    Added code to show the Screens in the MainView.swift and use MainView.swift under AfricaApp.swift as the entry screen to this Application.

--- Africa/App/AfricaApp.swift
+++ Africa/App/AfricaApp.swift
@@ -9,9 +9,18 @@ import SwiftUI
 
 @main
 struct AfricaApp: App {
+
     var body: some Scene {
+
         WindowGroup {
-            ContentView()
+
+            /*
+             MainView is the entry screen of this application.
+             */
+            MainView()
+
         }
+
     }
+
 }
--- Africa/UI/Screen/MainView.swift
+++ Africa/UI/Screen/MainView.swift
@@ -10,7 +10,38 @@ import SwiftUI
 struct MainView: View {
 
     var body: some View {
-        Text("Main View")
+
+        /*
+         Show a Tab like Navigation where User can navigate between different screens created.
+         */
+        TabView() {
+
+            ContentView() // Default Home Page
+                .tabItem {
+                    Image(systemName: "square.grid.2x2")
+                    Text("Browse")
+                }
+
+            VideoListView() // Page to show List of Videos.
+                .tabItem {
+                    Image(systemName: "play.rectangle")
+                    Text("Watch")
+                }
+
+            MapView() // Page to show the Locations.
+                .tabItem {
+                    Image(systemName: "map")
+                    Text("Locations")
+                }
+
+            GalleryView() // Page to show the Photo Gallery.
+                .tabItem {
+                    Image(systemName: "photo")
+                    Text("Gallery")
+                }
+
+        }
+
     }
 
 }
