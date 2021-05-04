94ffa5228bdae12907938781ded0b77773d65869
ritwikjamuar <ritwikjamuar@gmail.com>
2021-05-04 20:06:54 +0530

    Added CoverImageView.swift as a reusable component to show a Cover Image.

---
+++ Africa/UI/Component/CoverImageView.swift
@@ -0,0 +1,27 @@
+//
+//  CoverImageView.swift
+//  Africa
+//
+//  Created by Ritwik Jamuar on 04/05/21.
+//
+
+import SwiftUI
+
+/**
+ Reusable View to show a Cover Image.
+ */
+struct CoverImageView: View {
+
+    var body: some View {
+        Text("Hello, World!")
+    }
+
+}
+
+struct CoverImageView_Previews: PreviewProvider {
+
+    static var previews: some View {
+        CoverImageView()
+    }
+
+}
