4c02d2ee0fe1e96ff7cc1542bd8992b42f286162
ritwikjamuar <ritwikjamuar@gmail.com>
2021-05-04 19:34:17 +0530

    Added screens for the application.

---
+++ Africa/UI/Screen/MainView.swift
@@ -0,0 +1,24 @@
+//
+//  MainView.swift
+//  Africa
+//
+//  Created by Ritwik Jamuar on 04/05/21.
+//
+
+import SwiftUI
+
+struct MainView: View {
+
+    var body: some View {
+        Text("Main View")
+    }
+
+}
+
+struct MainView_Previews: PreviewProvider {
+
+    static var previews: some View {
+        MainView()
+    }
+
+}
