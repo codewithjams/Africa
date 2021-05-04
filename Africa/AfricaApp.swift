e876a89a1045a28bff4863853a73d2394bac90c2
ritwikjamuar <ritwikjamuar@gmail.com>
2021-05-04 15:32:50 +0530

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Ritwik Jamuar on 04/05/21.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
