c875eae87add650e21d8b570950ffd4a2666501c
ritwikjamuar <ritwikjamuar@gmail.com>
2021-05-09 21:14:11 +0530

    Added code in MapView.swift to show the Map in the View with the purpose to point out the Locations of the National Parks.

--- Africa/UI/Screen/MapView.swift
+++ Africa/UI/Screen/MapView.swift
@@ -6,11 +6,86 @@
 //
 
 import SwiftUI
+import MapKit
 
+/**
+ View as a screen to show the Map, pointing out the Locations of the National Parks.
+ */
 struct MapView: View {
 
+    /**
+     Constant Collection of Locations of National Parks, fetched from JSON File.
+     */
+    let locations: [Location] = Bundle.main.decodeJSON(file: "locations.json")
+
+    /**
+     Map Kit Coordinate Region as a State which maintains the state of our Map View during the Screen Lifecycle.
+     */
+    @State private var region: MKCoordinateRegion = {
+
+        // Create a 2D Location Coordinate to hover the Map over the Center of Africa.
+        var mapCoordinates = CLLocationCoordinate2D(
+            latitude: 6.600286,
+            longitude: 16.4377599
+        )
+
+        // Create a Map Kit Coordinate Span to set the Zoom Level of the Map View.
+        var mapZoomLevel = MKCoordinateSpan(
+            latitudeDelta: 70.0,
+            longitudeDelta: 70.0
+        )
+
+        // Create a region using 'mapCoordinates' and 'mapZoomLevel'.
+        var mapRegion = MKCoordinateRegion(
+            center: mapCoordinates,
+            span: mapZoomLevel
+        )
+
+        // Return the 'mapRegion'.
+        return mapRegion
+
+    } ()
+
     var body: some View {
-        Text("Map View")
+
+        // Show a Map in the View.
+        Map(
+            coordinateRegion: $region,
+            annotationItems: locations,
+            annotationContent: { location in
+
+                // Show a Map Pin, denoting the Pin in the Map.
+//                MapPin(
+//                    coordinate: location.location,
+//                    tint: .accentColor
+//                )
+
+                // Show a Map Marker, denoting the Mark Points.
+//                MapMarker(
+//                    coordinate: location.location,
+//                    tint: .accentColor
+//                )
+
+                // Show a Generic 'MapAnnotation' with our own customization to the icon shown in the Map.
+                MapAnnotation(
+                    coordinate: location.location // Set the Location in the Map.
+                ) {
+
+                    // Show an Image 'logo' which acts as the Icon of our 'MapAnnotation'.
+                    Image("logo")
+                        .resizable() // Make this Image resizable in nature.
+                        .scaledToFit() // Make this Image scaled to fit.
+                        .frame( // Set the Width, Height and Alignment of this Image.
+                            width: 32,
+                            height: 32,
+                            alignment: .center
+                        )
+
+                }
+
+            }
+        )
+
     }
 
 }
