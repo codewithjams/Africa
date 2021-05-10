521b061ab34d08b4ea5767fb52f52542667d9323
ritwikjamuar <ritwikjamuar@gmail.com>
2021-05-10 16:59:07 +0530

    Added code in `GalleryView.swift` to render the `MotionAnimationView.swift` as a background of the `ScrollView`.

--- Africa/UI/Screen/GalleryView.swift
+++ Africa/UI/Screen/GalleryView.swift
@@ -10,7 +10,20 @@ import SwiftUI
 struct GalleryView: View {
 
     var body: some View {
-        Text("Gallery View")
+
+        // Wrap the contents under Scroll View.
+        ScrollView(.vertical) {
+
+            // Show the Placeholder Text to denote Gallery.
+            Text("Gallery")
+
+        }.frame( // Set the frame dimensions of this ScrollView.
+            maxWidth: .infinity,
+            maxHeight: .infinity
+        ).background(
+            MotionAnimationView() // Set the background of this ScrollView as 'MotionAnimationView'.
+        )
+
     }
 
 }
